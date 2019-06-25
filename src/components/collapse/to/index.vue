e585787aedbafb1ed4e7a5024d61bd1ecf6e7b50
shizhiqiang <icrank@qq.com>
2019-06-25 23:46:47 +0800

    edit collapse _nice

--- src/components/collapse/to/index.vue
+++ src/components/collapse/to/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="collapse-mod">
     <div @click="change" class="title">title</div>
-    <div class='content' :style="{'height': height}" v-show="loading || show">
+    <div class='content' :style="{'height': height}" v-show="show">
       <div ref='box' class="box"><slot/></div>
     </div>
   </div>
@@ -13,41 +13,62 @@ export default {
   data() {
     return {
       show: false,
-      height: null,
-      loading: false,
-    }
-  },
-  watch: {
-    show(n) {
-      if (this.height !== null) return
-      this.loading = true
-      this.height = n ? 0 : this.$refs['box'].clientHeight + 'px'
-      setTimeout(() => {
-        this.height = n ? this.$refs['box'].clientHeight + 'px' : 0
-        setTimeout(() => {
-          this.height = null
-          this.loading = false
-        }, 200)
-      })
+      height: 0,
+      ST: null,
     }
   },
   methods: {
     change() {
-      if (this.loading) return
-      this.show = !this.show
+      // if (this.show) {
+      //   if (this.height === 0) {
+      //     clearInterval(this.ST)
+      //     this.show = true
+      //     setTimeout(() => {
+      //       const { clientHeight } = this.$refs['box']
+      //       this.height = clientHeight + 'px'
+      //     })
+      //   } else {
+      //     this.height = 0
+      //     this.ST = setTimeout(() => {
+      //       this.show = false
+      //     }, 300)
+      //   }
+      // } else {
+      //   this.show = true
+      //   setTimeout(() => {
+      //     const { clientHeight } = this.$refs['box']
+      //     this.height = clientHeight + 'px'
+      //   })
+      // }
+      if (this.show && this.height !== 0) {
+        this.height = 0
+        this.ST = setTimeout(() => {
+          this.show = false
+        }, 300)
+      } else {
+        if (this.height === 0) clearInterval(this.ST)
+        this.show = true
+        setTimeout(() => {
+          const { clientHeight } = this.$refs['box']
+          this.height = clientHeight + 'px'
+        })
+      }
     }
   }
 }
 </script>
 
 <style scoped>
+.collapse-mod{
+  border: 1px solid #dcdee2;
+}
 .collapse-mod .title{
-  background: aqua;
+  background: #f7f7f7;
   margin: 0;
-  padding: 10px 15px;
+  padding: 10px 16px;
+  font-size: 12px;
 }
 .collapse-mod .content{
-  background: rebeccapurple;
   transition: all .2s ease-in-out;
   will-change: height;
   overflow: hidden;
@@ -55,6 +76,9 @@ export default {
   padding-bottom: 0px;
 }
 .collapse-mod .content .box{
-  padding: 15px;  
+  padding: 16px;
+  color: #515a6e;
+  line-height: 1.5;
+  font-size: 14px;
 }
 </style>
